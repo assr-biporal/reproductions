8a7648f86a4450ca6240f1686ab045ce8380221b
yi1306c12 <yi1306c12@gmail.com>
2020-02-06 00:39:17 +0900

    load neurons

---
+++ Effects of neuromodulation in a cortical network model of object working memory dominated by recurrent inhibition/load_models.py
@@ -0,0 +1,33 @@
+import brian2
+import collections
+
+def parse_namespace(namespace_dict):
+    return {name : eval(quantity, brian2.core.namespace.DEFAULT_UNITS) for name, quantity in namespace_dict.items()}
+
+
+def load_neurongroups(neuron_model: str, neuron_params_dict: dict):
+    group_dict = dict()
+    for neuron_name, neuron_params in neuron_params_dict.items():
+        if 'namespace' in neuron_params:
+            neuron_namespace = parse_namespace(neuron_params.pop('namespace'))
+        else:
+            neuron_namespace = dict()
+
+        group_dict[neuron_name] = brian2.NeuronGroup(
+            neuron_params.pop('N'),
+            neuron_model,
+            name=neuron_name,
+            namespace=neuron_namespace,
+            **neuron_params
+        )
+    return group_dict
+
+
+if __name__ == '__main__':
+    import yaml
+    with open('neuron_model.txt', 'r') as model_f, open('neuron_parameters.yaml', 'r') as params_f:
+        neurongroup_dict = load_neurongroups(
+            model_f.read(),
+            yaml.safe_load(params_f)
+        )
+    print(neurongroup_dict)
